7b5624ea2187b2970d68d789ea60d3e7ee8e41d3
Troy Leach <troyleach29@gmail.com>
2016-08-07 16:40:14 -0500

    started on the front end, got the index done for the most part and the post show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,18 @@
 class PostsController < ApplicationController
+  include PostsHelper
+
   def index
-    @page_title = '{ blog: { "by": "TROYleach" } }'
+    posts = Post.all
+    @page_title = '{ "blog": { "by": "TROYleach" } }'
     @css_sytle = 'main-page-title'
+    @categories = Category.all
+    @main_blog = get_current_blog(posts)
+    @group_of_blogs = get_four_random_blogs(posts)
   end
 
   def show
-    @page_title = 'Ruby ON the Rails'
+    @blog = Post.find_by_id(params[:id])
+    @page_title = @blog.category.name
     @css_sytle = 'main-blog-title'
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,38 @@
 module PostsHelper
+
+  def get_current_blog(posts)
+    posts = posts.sort_by { |date| date.created_at }
+    return posts.last
+  end
+
+  def blog_date(blogs_date)
+    parsed_date = "Date: #{blogs_date.to_time.strftime('%B %e, %Y at %l:%M %p')}"
+    return parsed_date
+  end
+
+  def get_four_random_blogs(posts)
+    data = {
+      :group_one => nil,
+      :group_two => nil
+    }
+    posts = posts.sort_by { |date| date.created_at }.to_a
+    posts.pop
+    posts = posts.shuffle
+    posts = posts.slice(1..4)
+    posts = posts.shuffle
+
+    # data.each do |key, value|
+    #   posts.each_slice(2) do |value|
+    #     data[key] = value
+    #   end
+    # end
+    #refactor this so it makes more sense
+    blogs = {}
+    posts.each_slice(2).with_index do |value, index|
+      blogs[index + 1] = value
+    end
+    return blogs
+  end
+
 end
+
